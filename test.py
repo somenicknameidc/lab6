a18dac09cd0ed00563d47169f2ea715641e784a6
somenicknameidc <113014129+somenicknameidc@users.noreply.github.com>
2022-12-01 23:35:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ from main import calc
 
 class calcTest(unittest.TestCase):
     def test_first(self):
-        self.assertEqual(('1 2'), 3)
+        self.assertEqual(calc('1 2'), 3)
     
     def test_second(self):
-        self.assertEqual(('3 4'), 7)
+        self.assertEqual(calc('3 4'), 7)
     
     def test_third(self):
-        self.assertEqual(('100 200'), 300)
+        self.assertEqual(calc('100 200'), 300)
     
     def test_fourth(self):
-        self.assertEqual(('-1 2'), 2)
+        self.assertEqual(calc('-1 2'), 2)
