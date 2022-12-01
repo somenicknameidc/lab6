f4d86b22684380e5180a6cf49de280673a0c7454
somenicknameidc <113014129+somenicknameidc@users.noreply.github.com>
2022-12-01 23:10:31 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ import unittest
 from main import calc
 
 class calcTest(unittest.TestCase):
-  def test_first(self):
-    self.assertEqual(('1 2'), 3)
+    def test_first(self):
+        self.assertEqual(('1 2'), 3)
     
-   def test_second(self):
-    self.assertEqual(('3 4'), 7)
+    def test_second(self):
+        self.assertEqual(('3 4'), 7)
     
-  def test_third(self):
-    self.assertEqual(('100 200'), 300)
+    def test_third(self):
+        self.assertEqual(('100 200'), 300)
     
-  def test_fourth(self):
-    self.assertEqual(('-1 2'), 2)
+    def test_fourth(self):
+        self.assertEqual(('-1 2'), 2)
