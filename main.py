4bd2053aedeb77b2809f1623ff845c70b7e0c955
somenicknameidc <113014129+somenicknameidc@users.noreply.github.com>
2022-12-01 23:15:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
 def calc(expression):
     try:
-        a, b =  expression.split()
+        a, b = expression.split()
+        a, b = (int) a, (int) b
+        
         return (a + b)
     except(ValueError, TypeError):
-                raise ValueError('go away idiot')
+        raise ValueError('go away idiot')
 if __name__ == '__main__':
     calc('')
